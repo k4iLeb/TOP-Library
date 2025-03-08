2aa40a6fc66d96592e4377b1fdea785f295c3a6a
k4ileb <40836142+k4iLeb@users.noreply.github.com>
2025-03-08 03:48:12 +0200

    Finish refactoring to classes

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
-const books = [];
-
-// **** SELECTORS ****
-
-const input = document.querySelectorAll("input");
-const submit = document.querySelector(".submit");
-const inputCard = document.querySelector(".input");
-const addBook = document.querySelector(".addBook");
-const library = document.querySelector(".library");
-
-// **** LISTENERS ****
-
-submit.addEventListener("click", addBookToLibrary);
-
-addBook.addEventListener("click", showForm);
-
-document.addEventListener("click", (e) => {
-  if (inputCard.contains(e.target) || addBook.contains(e.target)) {
-  } else {
-    resetInput();
-  }
-});
-
-document.addEventListener("keydown", (e) => {
-  if (e.key == "Escape") resetInput();
-});
-
 // **** CLASSES ****
-
 class Book {
   constructor(title, author, pages, read) {
     this.title = title;
@@ -39,7 +11,7 @@ class Book {
       this.createData();
   }
 
-  createData() {
+  createData = () => {
     const date = new Date();
     const [year, month, day, hrs, mins, secs, ms] = [
       date.getFullYear(),
@@ -61,112 +33,156 @@ class Book {
     ];
 
     return data.join("");
-  }
+  };
 
   toggleRead = () => {
-    return (this.read = !this.read);
+    this.read = !this.read;
   };
 }
 
-books.push(
-  new Book("Atomic Habits", "James Clear", "320", false),
-  new Book("So Good They Can't Ignore You", "Cal Newport", "305", false),
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  addBook = (book) => {
+    this.books.push(book);
+  };
+
+  removeBook = (id) => {
+    this.books = this.books.filter((book) => book.data !== id);
+  };
+
+  get getBooks() {
+    return this.books;
+  }
+}
+
+class Display {
+  constructor(library) {
+    this.library = library;
+  }
+
+  showForm = () => {
+    inputCard.style.visibility = "visible";
+  };
+
+  resetInput = () => {
+    input.forEach((x) => {
+      x.value = "";
+      x.checked = false;
+    });
+    inputCard.style.visibility = "hidden";
+  };
+
+  showBooks = () => {
+    libraryDiv.innerHTML = "";
+
+    this.library.getBooks.forEach((book) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      card.setAttribute("data", `${book.data}`);
+
+      const titleEl = document.createElement("p");
+      titleEl.classList.add("title");
+      titleEl.textContent = book.title;
+
+      const authorEl = document.createElement("p");
+      authorEl.classList.add("author");
+      authorEl.textContent = `by ${book.author}`;
+
+      const pagesEl = document.createElement("p");
+      pagesEl.classList.add("pages");
+      pagesEl.textContent = `${book.pages} pages`;
+
+      const readEl = document.createElement("div");
+      readEl.classList.add("status");
+
+      const readSp = document.createElement("span");
+      readSp.classList.add("status");
+      readSp.textContent = book.read ? "Read" : "Not Read";
+      readSp.style.backgroundColor = book.read
+        ? "rgba(68, 250, 129, 0.699)"
+        : "rgba(250, 68, 92, 0.699)";
+
+      const delEl = document.createElement("button");
+      delEl.classList.add("del-button");
+      delEl.innerText = `X`;
+      delEl.addEventListener("click", (e) => {
+        this.library.getBooks.forEach((x) => {
+          let data = e.target.parentElement.attributes[1].value;
+          if (x.data == data) {
+            this.library.removeBook(data);
+          }
+        });
+        display.showBooks();
+      });
+
+      const toggleButton = document.createElement("button");
+      toggleButton.classList.add("toggle-btn");
+      toggleButton.textContent = book.read ? "Mark as Unread" : "Mark as Read";
+
+      toggleButton.addEventListener("click", (e) => {
+        const data = e.target.parentElement.parentElement.attributes[1].value;
+        const selectedBook = this.library.getBooks.find((x) => x.data === data);
+        selectedBook.toggleRead();
+        display.showBooks();
+      });
+
+      readEl.append(readSp, toggleButton);
+      card.append(titleEl, authorEl, pagesEl, readEl, delEl);
+      libraryDiv.appendChild(card);
+    });
+  };
+}
+
+// **** INITIALIZATION ****
+const myLibrary = new Library();
+const display = new Display(myLibrary);
+
+myLibrary.addBook(new Book("Atomic Habits", "James Clear", "320", false));
+myLibrary.addBook(
+  new Book("So Good They Can't Ignore You", "Cal Newport", "305", false)
+);
+myLibrary.addBook(
   new Book("The Subtle Art of Not Giving a F*ck", "Mark Manson", "212", true)
 );
 
-// console.log(books);
+// **** SELECTORS ****
 
-// console.log(new Book("Atomic Habits", "James Clear", "320", false));
+const input = document.querySelectorAll("input");
+const submit = document.querySelector(".submit");
+const inputCard = document.querySelector(".input");
+const addBook = document.querySelector(".addBook");
+const libraryDiv = document.querySelector(".library");
 
-// **** FUNCTIONS ****
+// **** LISTENERS ****
 
-function showForm() {
-  inputCard.style.visibility = "visible";
-}
-
-function addBookToLibrary() {
+submit.addEventListener("click", () => {
   const book = new Book(
     input[0].value,
     input[1].value,
     input[2].value,
     input[3].checked
   );
+  myLibrary.addBook(book);
+  display.resetInput();
+  display.showBooks();
+});
 
-  books.push(book);
-  resetInput();
+addBook.addEventListener("click", display.showForm);
 
-  displayBooks();
-}
+document.addEventListener("click", (e) => {
+  if (inputCard.contains(e.target) || addBook.contains(e.target)) {
+  } else {
+    display.resetInput();
+  }
+});
 
-function resetInput() {
-  input.forEach((x) => {
-    x.value = "";
-    x.checked = false;
-  });
-  inputCard.style.visibility = "hidden";
-}
-
-function displayBooks() {
-  library.innerHTML = "";
-  books.forEach((book) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.setAttribute("data", `${book.data}`);
-
-    const titleEl = document.createElement("p");
-    titleEl.classList.add("title");
-    titleEl.textContent = book.title;
-
-    const authorEl = document.createElement("p");
-    authorEl.classList.add("author");
-    authorEl.textContent = `by ${book.author}`;
-
-    const pagesEl = document.createElement("p");
-    pagesEl.classList.add("pages");
-    pagesEl.textContent = `${book.pages} pages`;
-
-    const readEl = document.createElement("div");
-    readEl.classList.add("status");
-
-    const readSp = document.createElement("span");
-    readSp.classList.add("status");
-    readSp.textContent = book.read ? "Read" : "Not Read";
-    readSp.style.backgroundColor = book.read
-      ? "rgba(68, 250, 129, 0.699)"
-      : "rgba(250, 68, 92, 0.699)";
-
-    const delEl = document.createElement("button");
-    delEl.classList.add("del-button");
-    delEl.innerText = `X`;
-    delEl.addEventListener("click", (e) => {
-      books.forEach((x, index) => {
-        let data = e.target.parentElement.attributes[1].value;
-        if (x.data == data) {
-          books.splice(index, 1);
-          displayBooks();
-        }
-      });
-    });
-
-    const toggleButton = document.createElement("button");
-    toggleButton.classList.add("toggle-btn");
-    toggleButton.textContent = read ? "Mark as Unread" : "Mark as Read";
-
-    toggleButton.addEventListener("click", (e) => {
-      const data = e.target.parentElement.parentElement.attributes[1].value;
-      books.forEach((x) => {
-        if (x.data == data) {
-          x.toggleRead();
-          displayBooks();
-        }
-      });
-    });
-
-    readEl.append(readSp, toggleButton);
-    card.append(titleEl, authorEl, pagesEl, readEl, delEl);
-    library.appendChild(card);
-  });
-}
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Escape") display.resetInput();
+});
 
 // **** DISPLAY BOOKS ****
-displayBooks();
+
+display.showBooks();
